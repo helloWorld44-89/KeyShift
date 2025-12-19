1577b3d7b80e559d3102f01e0d93e0d6cec4aaec
jconder44 <m40a3lphoenix@gmail.com>
2025-12-18 18:26:38 -0600

    added coment to cron for pw change

--- app/cron.py
+++ app/cron.py
@@ -6,6 +6,6 @@ import datetime
 now=datetime.datetime.now()
 pw = genPW()
 genQRCode(pw)
-
+### Added API Call code here for changing the password
 with open("/app/app/cron.log","a") as file:
     file.write(f"{now}|PW: {pw} | QRCode created ")
